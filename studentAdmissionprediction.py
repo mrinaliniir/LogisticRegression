fdaea78bcd59d7696a6e1ef226909e6484c5a4c6
Mrinalini <ir.mrinalini@gmail.com>
2019-11-02 13:33:18 -0400

    added evaluation metrics(ROC, AUC, accuracy, precison, recall)

--- studentAdmissionprediction.py
+++ studentAdmissionprediction.py
@@ -6,12 +6,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model.logistic import _logistic_loss
 from sklearn.model_selection import train_test_split
 from math import sqrt
-from sklearn.metrics import mean_squared_error
-#from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc
+
 
 # load the dataset
-df = pd.read_csv("C:/Users/irmri/OneDrive/Desktop/coursera/machine-learning-ex2/ex2/ex2data1.txt")
+df = pd.read_csv("C:/Users/irmri/OneDrive/Documents/academic_projects/coursera/StudentAdmissionPrediction-master/ex2data1.txt")
 df.columns=['x1','x2','y']
 
 # visulaizing the dataset
@@ -34,6 +39,7 @@ mean = X.mean(axis=0)
 sd = X.std(axis=0)
 J_history =[]
 
+# feature standardization
 def normalizeFeatures(mean, sd, features):
 		features['x1'] = (features.x1 - mean.x1)/sd.x1
 		features['x2'] = (features.x2 - mean.x2)/sd.x2
@@ -41,9 +47,11 @@ def normalizeFeatures(mean, sd, features):
 
 x = normalizeFeatures(mean, sd, X)
 
+# sigmoid function
 def sigmoid(z):
 	return 1/ (1 + np.exp(-z))
 
+#loss function 
 def costFunction(initial_theta, X , y):
 	m=len(y)
 	predictions = sigmoid(np.dot(X,initial_theta))
@@ -51,6 +59,7 @@ def costFunction(initial_theta, X , y):
 	cost = -1/m * sum(error)
 	return cost
 
+#gradient descent
 def gradientDescent(alpha, iterations, theta, X, y):
 	for i in range(iterations):
 		z = X.dot(theta.T)
@@ -77,33 +86,47 @@ def predictModel(theta, X):
 	predictions = sigmoid(np.dot(X,theta))  # Get predictions
 	return predictions
 
-# Calculate the mean squared error
-def rmse(X_train, y_train, theta):
-    predicted_value = predictModel(theta, X_train)
-    actual_value = y_train
-    mean_error = np.sqrt(np.mean((predicted_value - actual_value)**2))
-    return mean_error
 
 print("Training model .....")
 weights = trainModel(X_train, y_train)
+y_pred_LR = predictModel(weights,X_test).round()
 print("********Results for logistic regression using current model**********")
-RMSE_train = rmse(X_train, y_train, weights)
-print("RMSE train :", RMSE_train)
-RMSE_test = rmse(X_test, y_test, weights)
-print("RMSE test :", RMSE_test) 
-print(weights)
-print("******************")
+CM_LR = confusion_matrix(y_test,y_pred_LR)
+print("Confusion Matrix")
+print(CM_LR)
+accuracy_LR = accuracy_score(y_test,y_pred_LR)
+print("Accuracy of predictions :",accuracy_LR)
+precision_LR = precision_score(y_test,y_pred_LR)
+print("Precison of predictions :",precision_LR)
+recall_LR = recall_score(y_test,y_pred_LR)
+print("Recall :",recall_LR)
+
+fpr,tpr,thresholds = roc_curve(y_test,y_pred_LR)
+plt.plot(fpr,tpr,label= "Logistic Regression")
+plt.xlabel("False Positive Rate(FPR)")
+plt.ylabel("True Positive Rate(TPR)")
+plt.legend()
+plt.show()
+print("AUC for Logistic Regression ",auc(fpr,tpr))
+print("*********************************************************************")
+
+
 
 
 model = LogisticRegression()
 model.fit(X_train,y_train)
 print("******** Results for logistic regression using scikit learn library **********")
 y_pred = model.predict(X_test)
-RMSE2 = sqrt(mean_squared_error(y_test, y_pred))
-print("RMSE :", RMSE2)
-print(model.coef_)
-print(model.intercept_)
-print("******************")
+CM = confusion_matrix(y_test,y_pred)
+print("Confusion Matrix")
+print(CM)
+accuracy = accuracy_score(y_test,y_pred)
+print("Accuracy of predictions :",accuracy)
+precision = precision_score(y_test,y_pred)
+print("Precison of predictions :",precision)
+recall = recall_score(y_test,y_pred)
+print("Recall :",recall)
+print("*********************************************************************")
 	
 
 
